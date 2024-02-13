ef3fc055fed8d89e6dd6718f68963c254a5d7e20
Sora <dart238@email.com>
2024-02-13 15:40:43 -0500

    lising works

--- src/services/animalService.js
+++ src/services/animalService.js
@@ -2,22 +2,40 @@ import axios from 'axios'
 
 const BASE_URL = `${process.env.REACT_APP_BACK_END_SERVER_URL}/animals/`
 
-async function getAnimals() {
+// async function getAnimals() {
+//     try {
+//         const token = localStorage.getItem('token')
+//         const res = await axios.get(`${BASE_URL}`, {
+//             method: 'GET',
+//             headers: {
+//                 'Content-Type': 'application/json',
+//                 'Authorization': `Bearer ${token}`
+//             }
+//         });
+
+//         const animals = res.data;
+//         console.log(animals);
+//         return animals;
+//     } catch (error) {
+//         console.error('Error fetching animals:', error);
+//     }
+// }
+
+async function getAnimals(filters = {}) {
+    const token = localStorage.getItem('token');
+    const queryString = new URLSearchParams(filters).toString();
+    const url = `${BASE_URL}?${queryString}`;
     try {
-        const token = localStorage.getItem('token')
-        const res = await axios.get(`${BASE_URL}`, {
-            method: 'GET',
+        const response = await axios.get(url, {
             headers: {
                 'Content-Type': 'application/json',
-                'Authorization': `Bearer ${token}`
-            }
+                'Authorization': `Bearer ${token}`,
+            },
         });
-
-        const animals = res.data;
-        console.log(animals);
-        return animals;
+        return response.data;
     } catch (error) {
         console.error('Error fetching animals:', error);
+        throw error;
     }
 }
 
