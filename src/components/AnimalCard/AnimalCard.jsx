7cf6f5dd7d01ca71bb46224d15a680d487e0a3d1
Sora <dart238@email.com>
2024-02-13 16:12:01 -0500

    fixed card , created details

--- src/components/AnimalCard/AnimalCard.jsx
+++ src/components/AnimalCard/AnimalCard.jsx
@@ -1,24 +1,25 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
 
 const AnimalCard = ({ animal}) => {
 
  const displayImage = animal.photos.length > 0 ? animal.photos[0].medium : null;
 
   return (
+   
     // <div className="animal-card">
-    //   <img src={animal.image} alt={animal.name}/>
+    //   {displayImage && <img src={displayImage} alt={`Photo of ${animal.name}`} />}
     //   <h2>{animal.name}</h2>
-    //   <p>Type: {animal.type}</p>
     //   <p>Age: {animal.age}</p>
-    //   {/* More to be included will make a group check on monday */}
+    //   <p>Location: {animal.location}</p>
     // </div>
-    <div className="animal-card">
+    <Link to={`/animals/${animal.id}`} className="animal-card">
       {displayImage && <img src={displayImage} alt={`Photo of ${animal.name}`} />}
       <h2>{animal.name}</h2>
       <p>Age: {animal.age}</p>
       <p>Location: {animal.location}</p>
-    </div>
-
+      {/* Add more details as per your requirement */}
+    </Link>
 
   )
 }
